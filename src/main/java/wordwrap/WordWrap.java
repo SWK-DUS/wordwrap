0a39f5c3c06c4e990287365f472b498b8217d0f2
Georg Berky <georg.berky@valtech.de>
2016-11-23 19:13:55 +0100

    guard clause in wrap

--- src/main/java/wordwrap/WordWrap.java
+++ src/main/java/wordwrap/WordWrap.java
@@ -2,15 +2,16 @@ package wordwrap;
 
 public class WordWrap {
     public static String wrap(String text, int length) {
-        if(text.length() > length) {
-            int indexOfFirstSpace = text.indexOf(" ");
-            if(indexOfFirstSpace == -1) {
-                indexOfFirstSpace = length;
-            }
-
-            return text.substring(0, indexOfFirstSpace).trim() + "\n" +
-                    wrap(text.substring(indexOfFirstSpace).trim(),length);
+        if (text.length() <= length) {
+            return text;
         }
-        return text;
+
+        int indexOfFirstSpace = text.indexOf(" ");
+        if(indexOfFirstSpace == -1) {
+            indexOfFirstSpace = length;
+        }
+
+        return text.substring(0, indexOfFirstSpace).trim() + "\n" +
+                wrap(text.substring(indexOfFirstSpace).trim(),length);
     }
 }
